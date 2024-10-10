0c9f8d0a8c62ca6728c444431be7d2f0e915a046
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2024-10-10 14:57:24 +0700

    add header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,10 @@
+<header class="max-w-screen-md mx-auto px-5 py-8 border-b">
+	<p class="text-xl font-medium">Muhammad <span class="text-lime-600">Hafid</span> Khoirul</p>
+	<nav class="text-sm flex gap-4 mt-2">
+		<a href="/about" class="hover:text-lime-700">About</a>
+		<p>|</p>
+		<a href="/blog" class="hover:text-lime-700">Blog</a>
+		<p>|</p>
+		<a href="/#contact" class="hover:text-lime-700">Contact</a>
+	</nav>
+</header>
