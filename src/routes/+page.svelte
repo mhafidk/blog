4e4a616ae1dfc3b441e7ba9f62345ecdba3491e5
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2024-10-10 15:14:01 +0700

    add index hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<div class="">
-	<h1>Muhammad Hafid Khoirul</h1>
+<div class="mt-16">
+	<p class="text-4xl">Hi! I am <span class="text-lime-600 font-medium">Hafid</span> 👋</p>
+	<p class="text-4xl font-bold my-3">Software Developer</p>
+	<p class="text-2xl">
+		Currently, I am working as a backend engineer at <a
+			href="https://www.qontak.com"
+			target="_blank"
+			rel="noreferrer"
+			class="border-b border-lime-600 text-lime-600 hover:text-lime-700">Qontak</a
+		>.
+	</p>
 </div>
