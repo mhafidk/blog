5ca00836f8969961a8a206b9e553a2ce11448988
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2024-10-10 16:43:28 +0700

    add a little about me section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="mt-16">
-	<p class="text-4xl">Hi! I am <span class="text-lime-600 font-medium">Hafid</span> 👋</p>
-	<p class="text-4xl font-bold my-3">Software Developer</p>
+	<h1 class="text-4xl">Hi! I am <span class="text-lime-600 font-medium">Hafid</span> 👋</h1>
+	<p class="text-4xl font-bold my-3">Software Engineer</p>
 	<p class="text-2xl">
 		Currently, I am working as a backend engineer at <a
 			href="https://www.qontak.com"
@@ -8,5 +8,17 @@
 			rel="noreferrer"
 			class="border-b border-lime-600 text-lime-600 hover:text-lime-700">Qontak</a
 		>.
+	</p>
+</div>
+<div class="my-20">
+	<h2 class="text-2xl font-medium">A Little About Me</h2>
+	<p class="mt-4">
+		I am a software engineer based in Bogor, Indonesia. Currently, I am focused on backend
+		technology, where I design efficient and scalable systems. My expertise includes building robust
+		APIs, crafting optimized queries, and writing high-performance, well-tested, maintainable code.
+	</p>
+	<p class="mt-2">
+		Outside of development, I’m an amateur photographer and a writer. I enjoy blending creativity
+		and logic in both tech and life.
 	</p>
 </div>
