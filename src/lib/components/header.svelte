bc11281d7d9d017192fc0ad9530d3fb0f291b00f
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2024-10-13 15:29:47 +0700

    little adjustments

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,8 +1,8 @@
-<header class="max-w-screen-md mx-auto px-5 py-8 border-b">
+<header class="max-w-screen-md mx-auto px-5 py-8 border-b text-center">
 	<a href="/" class="text-xl font-medium"
 		>Muhammad <span class="text-lime-600">Hafid</span> Khoirul</a
 	>
-	<nav class="text-sm flex gap-4 mt-2">
+	<nav class="text-sm flex gap-4 mt-2 justify-center">
 		<a href="/about" class="hover:text-lime-700">About</a>
 		<p>|</p>
 		<a href="/blog" class="hover:text-lime-700">Blog</a>
