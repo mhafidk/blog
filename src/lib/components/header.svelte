9054aa7fe82a523fb9134c4d7acdf24c0c30616f
mhafidk <mhafidk@gmail.com>
2025-06-27 22:06:40 +0700

    adjust navbar

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
 <header>
 	<nav class="pt-6 pb-14">
 		<div class="flex flex-col items-center justify-center gap-4 sm:flex-row sm:justify-start">
@@ -11,9 +15,28 @@
 			<a href="/" class="text-2xl font-bold hover:text-lime-700">Muhammad Hafid K</a>
 		</div>
 		<div class="mt-4 flex justify-center gap-6 text-sm sm:justify-start">
-			<a href="/about" class="hover:text-lime-700">About</a>
-			<a href="/blog" class="hover:text-lime-700">Blog</a>
-			<a href="/#contact" class="hover:text-lime-700">Contact</a>
+			<a
+				href="/"
+				class="border-b-2 border-transparent pb-4 hover:text-lime-600"
+				class:text-lime-600={$page.url?.pathname === '/'}
+				class:border-lime-600={$page.url?.pathname === '/'}
+				class:border-transparent={$page.url?.pathname !== '/'}>Home</a
+			>
+			<a
+				href="/about"
+				class="border-b-2 border-transparent pb-4 hover:text-lime-600"
+				class:text-lime-600={$page.url?.pathname === '/about'}
+				class:border-lime-600={$page.url?.pathname === '/about'}
+				class:border-transparent={$page.url?.pathname !== '/about'}>About</a
+			>
+			<a
+				href="/blog"
+				class="border-b-2 border-transparent pb-4 hover:text-lime-600"
+				class:text-lime-600={$page.url?.pathname.includes('/blog')}
+				class:border-lime-600={$page.url?.pathname.includes('/blog')}
+				class:border-transparent={!$page.url?.pathname.includes('/blog')}>Blog</a
+			>
+			<a href="/#contact" class="border-b-2 border-transparent pb-4 hover:text-lime-600">Contact</a>
 		</div>
 	</nav>
 </header>
