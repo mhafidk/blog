23045bde6f7c21d89bce059a2e0c18a40d4125e3
mhafidk <mhafidk@gmail.com>
2025-06-27 13:16:08 +0700

    add header component

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,15 @@
+<header>
+	<nav>
+		<div class="flex items-center gap-4">
+			<a href="/"
+				><img src="mhk-potrait.webp" class="h-8 w-8 rounded-full" alt="Muhammad Hafid K" /></a
+			>
+			<a href="/" class="text-2xl font-bold hover:text-lime-700">Muhammad Hafid K</a>
+		</div>
+		<div class="mt-3 flex gap-6">
+			<a href="/about" class="hover:text-lime-700">About</a>
+			<a href="/blog" class="hover:text-lime-700">Blog</a>
+			<a href="/#contact" class="hover:text-lime-700">Contact</a>
+		</div>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import Header from '$lib/components/header.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-<div class="font-display">
-	{@render children()}
+<div class="font-display bg-white text-neutral-900">
+	<div class="px- mx-auto max-w-2xl px-6 py-8">
+		<Header />
+		{@render children()}
+	</div>
 </div>
