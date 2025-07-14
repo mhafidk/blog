390fc9d1a607a73f4345da03de1ebfb3513e4b62
mhafidk <mhafidk@gmail.com>
2025-07-14 21:50:47 +0700

    add book links

---
+++ src/lib/components/link.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { link, text } = $props();
+</script>
+
+<a
+	target="_blank"
+	class="w-fit border-b border-b-lime-600 text-lime-600 hover:border-b-lime-700 hover:text-lime-700"
+	href={`${link}`}>{text}</a
+>
--- src/routes/smambo/+page.svelte
+++ src/routes/smambo/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Link from '$lib/components/link.svelte';
 	import MateriTugas from '$lib/components/materi-tugas.svelte';
 
 	export let data: {
@@ -31,6 +32,27 @@
 	<h2 class="text-2xl font-bold">SMA Muhammadiyah Bojonggede</h2>
 	<div class="mt-6">
 		<div class="">
+			<h3 class="text-xl font-bold">Buku</h3>
+			<div class="mt-3 grid grid-cols-1 gap-2">
+				<Link
+					text="Matematika Lanjut Kelas 11"
+					link="https://drive.google.com/file/d/1IcdKqqVT18F4806XODMLkiHwkG9xHljg/view"
+				/>
+				<Link
+					text="Matematika Lanjut Kelas 12"
+					link="https://drive.google.com/file/d/1d3POAuKP2qW3tERdhh_v6jTT-Xo9HY08/view"
+				/>
+				<Link
+					text="Matematika Umum Kelas 11"
+					link="https://drive.google.com/file/d/1TWCsrE6y5IHpmvx7arK-H9NPn_JvGbIf/view"
+				/>
+				<Link
+					text="Matematika Umum Kelas 12"
+					link="https://drive.google.com/file/d/1TU5fsngZuPfsF-5xJ4Q5UoYGvsMEq1uz/view"
+				/>
+			</div>
+		</div>
+		<div class="mt-6">
 			<h3 class="text-xl font-bold">Tugas</h3>
 			<div class="mt-3">
 				<div class="">
