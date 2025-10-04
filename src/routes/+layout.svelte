d04c6689fe5e2d4c3087acf47fa3d696ed35d7ec
mhafidk <mhafidk@gmail.com>
2025-10-04 13:25:00 +0700

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<div class="mx-auto max-w-3xl p-4">
+	<header class="pt-8">
+		<h1>
+			<a href="/" class="text-xl font-semibold text-lime-600 hover:text-lime-700"
+				>Muhammad Hafid K</a
+			>
+		</h1>
+		<div class="flex gap-4 pt-2 text-sm text-neutral-700">
+			<a href="/" class="hover:text-lime-700">Home</a>
+			<a href="/about" class="hover:text-lime-700">About</a>
+			<a href="/#contact" class="hover:text-lime-700">Contact</a>
+		</div>
+	</header>
+	<main class="py-12">
+		{@render children?.()}
+	</main>
+</div>
