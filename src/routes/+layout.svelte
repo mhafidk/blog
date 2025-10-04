b6c639495fd51d3bb48a21efe0dfa7c8ef478405
mhafidk <mhafidk@gmail.com>
2025-10-04 13:52:45 +0700

    add index hero section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,14 +12,14 @@
 <div class="mx-auto max-w-3xl p-4">
 	<header class="pt-8">
 		<h1>
-			<a href="/" class="text-xl font-semibold text-lime-600 hover:text-lime-700"
+			<a href="/" class="text-xl font-semibold text-violet-600 hover:text-violet-700"
 				>Muhammad Hafid K</a
 			>
 		</h1>
 		<div class="flex gap-4 pt-2 text-sm text-neutral-700">
-			<a href="/" class="hover:text-lime-700">Home</a>
-			<a href="/about" class="hover:text-lime-700">About</a>
-			<a href="/#contact" class="hover:text-lime-700">Contact</a>
+			<a href="/" class="hover:text-violet-700">Home</a>
+			<a href="/#about" class="hover:text-violet-700">About</a>
+			<a href="/#contact" class="hover:text-violet-700">Contact</a>
 		</div>
 	</header>
 	<main class="py-12">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>Muhammad Hafid Khoirul</title>
+</svelte:head>
+
+<div class="">
+	<p>Hi there! 👋</p>
+	<p class="pt-2">
+		I'm <span class="font-medium text-violet-600">Hafid</span>, software engineer. Currently, I'm
+		working as a <span class="font-medium text-violet-600">backend engineer</span> at
+		<a
+			href="https://www.qontak.com"
+			target="_blank"
+			class="text-violet-600 underline hover:text-violet-700">Qontak</a
+		>. I write blog and make videos about tech stuff.
+	</p>
+	<p class="pt-2">
+		<a href="/blog" class="text-violet-600 underline hover:text-violet-700">Checkout my blog</a> or
+		<a
+			href="https://www.youtube.com/@muhammadhafidk"
+			class="text-violet-600 underline hover:text-violet-700">checkout my youtube channel</a
+		>!
+	</p>
+	<p class="pt-2">
+		Outside of development, I'm an amateur photographer. I enjoy blending creativity and logic in
+		both tech and life.
+	</p>
+</div>
