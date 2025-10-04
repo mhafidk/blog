d7f035b468de91e0cd7f960a7209b189dd4f13af
mhafidk <mhafidk@gmail.com>
2025-10-04 14:08:13 +0700

    add blog card

---
+++ src/lib/components/blog-card.svelte
@@ -0,0 +1,15 @@
+<div class="rounded bg-neutral-100">
+	<div class="px-6 py-4">
+		<a href="/blog" class="font-medium text-violet-600 hover:text-violet-700">Post Title</a>
+		<p class="line-clamp-3 pt-2 text-neutral-600">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi quo sint, in saepe,
+			molestias odio rem illo atque mollitia laborum nisi? Temporibus libero deserunt perspiciatis
+			voluptatum nesciunt numquam accusamus illo!
+		</p>
+		<div class="mt-3 flex gap-1 font-mono text-xs text-violet-700">
+			<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">#programming</p>
+			<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">#general</p>
+			<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">#go</p>
+		</div>
+	</div>
+</div>
