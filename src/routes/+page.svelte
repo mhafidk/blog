ef03ca70bef0cabd9c9f211bc95cfb7b7b74dd3f
mhafidk <mhafidk@gmail.com>
2025-10-04 14:06:52 +0700

    add blog section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,3 +25,50 @@
 		both tech and life.
 	</p>
 </div>
+<div class="mt-16">
+	<h2 class="text-lg font-medium">Latest Blog Posts</h2>
+	<div class="mt-4 grid grid-cols-1 gap-4">
+		<div class="rounded bg-neutral-100">
+			<div class="px-6 py-4">
+				<a href="/blog" class="font-medium text-violet-600 hover:text-violet-700">Post Title</a>
+				<p class="line-clamp-3 pt-2 text-neutral-600">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi quo sint, in saepe,
+					molestias odio rem illo atque mollitia laborum nisi? Temporibus libero deserunt
+					perspiciatis voluptatum nesciunt numquam accusamus illo!
+				</p>
+				<div class="mt-3 flex gap-1 font-mono text-xs text-violet-700">
+					<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">
+						#programming
+					</p>
+					<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">#general</p>
+					<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">#go</p>
+				</div>
+				<div class="mt-3 flex justify-end text-sm">
+					<a href="/blog" class="text-violet-600 underline hover:text-violet-700">Read more -&gt;</a
+					>
+				</div>
+			</div>
+		</div>
+		<div class="rounded bg-neutral-100">
+			<div class="px-6 py-4">
+				<a href="/blog" class="font-medium text-violet-600 hover:text-violet-700">Post Title</a>
+				<p class="line-clamp-3 pt-2 text-neutral-600">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi quo sint, in saepe,
+					molestias odio rem illo atque mollitia laborum nisi? Temporibus libero deserunt
+					perspiciatis voluptatum nesciunt numquam accusamus illo!
+				</p>
+				<div class="mt-3 flex gap-1 font-mono text-xs text-violet-700">
+					<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">
+						#programming
+					</p>
+					<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">#general</p>
+					<p class="rounded-full bg-violet-100 px-2 py-0.5 font-medium text-violet-700">#go</p>
+				</div>
+				<div class="mt-3 flex justify-end text-sm">
+					<a href="/blog" class="text-violet-600 underline hover:text-violet-700">Read more -&gt;</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
