c333a43b226af841be38a12c7dfbe3fe92020e20
mhafidk <mhafidk@gmail.com>
2026-01-19 11:37:37 +0700

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/components/header.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<main class="bg-neutral-50 font-display text-neutral-900">
+	<Header />
+	<div class="mx-auto max-w-2xl px-6 pt-12 text-lg">
+		{@render children()}
+	</div>
+</main>
