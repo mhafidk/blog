5f75a7253854577e480902d4e4c91de1dbbd099d
mhafidk <mhafidk@gmail.com>
2026-01-22 15:35:09 +0700

    add about page

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -2,7 +2,7 @@
 	<div class="mx-auto max-w-2xl px-6 pt-6 text-sm md:pt-12">
 		<a href="/" class="text-base font-semibold hover:text-violet-700">Muhammad Hafid K</a>
 		<nav class="mt-1 flex items-center gap-4">
-			<a href="/#about" class="hover:text-violet-700">About</a>
+			<a href="/about" class="hover:text-violet-700">About</a>
 			<a href="/blog" class="hover:text-violet-700">Blog</a>
 			<a href="/#contact" class="hover:text-violet-700">Contact</a>
 		</nav>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<div class="mt-12">
+	<h1 class="text-3xl font-bold">About</h1>
+	<div class="mt-6">
+		<p>Hi, how are you? I hope you are fine!</p>
+		<p class="mt-2">
+			My name is
+			<span class="font-medium text-violet-700">Muhammad Hafid Khoirul</span>, people usually call
+			me
+			<span class="font-medium text-violet-700">Hafid</span>.
+		</p>
+		<p class="mt-2">
+			I'm a software developer based in Bogor, Indonesia, currently working as a backend engineer at
+			<a
+				class="text-violet-700 hover:text-violet-800"
+				href="https://www.qontak.com"
+				target="_blank"
+				rel="norefferer">Qontak</a
+			>. I write blog and make videos about tech stuff to share about my views and opinions and also
+			as notes for what I have learned.
+		</p>
+		<p class="mt-2">
+			I'm focused on backend technology right now. Mainly use Ruby on Rails at the moment, tried a
+			lot of things, Go, Phoenix, NestJs, etc, love them. Beside web development, I also interested
+			in other fields like computer vision and IoT. Doing small projects in my spare time.
+		</p>
+		<p class="mt-2">
+			Outside of development, I take photos, write articles, plant my own food, and play badminton.
+		</p>
+		<p class="mt-2">Finally, thank you for visiting this site, means a lot.</p>
+	</div>
+</div>
