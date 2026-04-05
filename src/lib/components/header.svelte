527be423e597dd6d669fc0431d1dcac240e62a6c
mhafidk <mhafidk@gmail.com>
2026-04-05 07:35:35 +0700

    style: center align header text and navigation links

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,9 +1,9 @@
-<header class="bg-neutral-50">
+<header class="bg-neutral-50 text-center">
 	<div
 		class="mx-auto max-w-2xl border-b border-b-neutral-300 px-6 pt-6 pb-6 text-sm md:pt-12 md:pb-12"
 	>
 		<a href="/" class="text-base font-semibold hover:text-violet-700">Muhammad Hafid K</a>
-		<nav class="mt-1 flex items-center gap-4">
+		<nav class="mt-2 flex items-center justify-center gap-4">
 			<a href="/about" class="hover:text-violet-700">About</a>
 			<a href="/blog" class="hover:text-violet-700">Blog</a>
 			<a href="/#" class="hover:text-violet-700">Contact</a>
